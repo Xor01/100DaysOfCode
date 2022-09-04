a98c6df3d862ae684f8133c7fc976cff235dc484
Xor01 <ms3ccc71@gmail.com>
2022-09-04 22:10:28 +0300

    Update the algorithm

--- Day4/rock-paper-scissors.py
+++ Day4/rock-paper-scissors.py
@@ -30,25 +30,23 @@ userChoice = int(input("Enter 0 for rock, 1 for paper, 2 for scissors: "))
 
 computerChoice = random.randint(0,2)
 
-if(userChoice == computerChoice):
-    print("Tie.")
-elif (userChoice == 0) & (computerChoice == 1):
-    print("You lost")
-    
-elif (userChoice == 0) & (computerChoice == 2):
-    print("You won")
-    
-elif (userChoice == 1) & (computerChoice == 0):
-    print("You won")
+if userChoice >= 3 or userChoice < 0: 
+    print("You typed an invalid number, you lose!")
 
 elif (userChoice == 2) & (computerChoice == 0):
     print("You lost")
     
-elif (userChoice == 1) & (computerChoice == 2):
+elif (userChoice == 0) & (computerChoice == 2):
+    print("You won")
+
+elif userChoice > computerChoice:
+    print("You won")
+
+elif computerChoice > userChoice:
     print("You lost")
 
-elif (userChoice == 2) & (computerChoice == 1):
-    print("You won")
-    
+elif userChoice == computerChoice:
+    print("It's a draw")
+
 print(f"Your choice: {moves[userChoice]}\n")
 print(f"Computer choice: {moves[computerChoice]}")
