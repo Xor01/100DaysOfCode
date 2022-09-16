d2849bdc5d1432cf42cb09f61b9c7740b2cff47d
Xor01 <ms3ccc71@gmail.com>
2022-09-16 16:13:29 +0300

    Day 15
    Coffee Machine

--- Day15/main.py
+++ Day15/main.py
@@ -30,36 +30,75 @@ resources = {
     "coffee": 100,
 }
 
+cash = 0.00
+
 
 def get_report():
-    print(f"Water: {resources['water']}")
-    print(f"Milk: {resources['milk']}")
-    print(f"Coffee: {resources['coffee']}")
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${cash}")
+
+
+def check_ingredient(drink_name):
+    for item in MENU[drink_name]['ingredients']:
+        if MENU[drink_name]['ingredients'][item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
 
 
 def make_drink(drink_name):
-    ingredients = MENU[drink_name]
+    for item in MENU[drink_name]['ingredients']:
+        resources[item] -= MENU[drink_name]['ingredients'][item]
 
-    if ingredients['ingredients']['water'] >= resources['water']:
-        return "UnSufficient Water"
 
-    elif ingredients['ingredients']['milk'] >= resources['milk']:
-        return "UnSufficient Milk"
+def check_money(drink_name):
+    drink_price = MENU[drink_name]['cost']
 
-    elif ingredients['ingredients']['coffee'] >= resources['coffee']:
-        return "UnSufficient Coffee"
+    total_money = take_money(drink_name)
 
-    return f"Here's your {drink_name}"
+    if total_money - drink_price < 0:
+        print(f"Insufficient money, take it back")
+        return False
+
+    print(f"This Drink cost you ${drink_price} you get in return ${'{:.2f}'.format(total_money - drink_price)}")
+    return True
+
+
+def take_money(drink_name):
+
+    quarters = int(input("Enter Quarters: ")) * 0.25
+    dimes = int(input("Enter Dimes: ")) * 0.1
+    nickles = int(input("Enter Nickles: ")) * 0.05
+    penny = int(input("Enter Penny: ")) * 0.01
+    total_money = quarters + dimes + nickles + penny
+
+    return total_money
 
 
 def main():
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
-    if choice in MENU:
-        print("We will check")
-        print(make_drink(choice))
 
-    else:
-        print("Check your spelling")
+    while True:
+
+        choice = input("What would you like? (espresso/latte/cappuccino): ")
+
+        if choice in MENU:
+
+            if check_ingredient(choice):
+                if check_money(choice):
+                    make_drink(choice)
+                    global cash
+                    cash += MENU[choice]['cost']
+
+        elif choice == "report":
+            get_report()
+
+        elif choice == "off":
+            return
+
+        else:
+            print("Check your spelling")
 
 
 main()
