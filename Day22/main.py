1182a62df375607d87d94f6e5b3e3194291fec6f
Xor01 <ms3ccc71@gmail.com>
2022-09-23 13:00:37 +0300

    create up and down methods and setheading to 90

--- Day22/main.py
+++ Day22/main.py
@@ -8,4 +8,10 @@ screen.title("Pong Game")
 screen.bgcolor("black")
 screen.setup(600, 600)
 
+screen.listen()
+
+screen.onkeypress(paddle1.up, "Up")
+screen.onkeypress(paddle1.down, "Down")
+
+
 screen.exitonclick()
--- Day22/paddle.py
+++ Day22/paddle.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
 
+SPEED = 0
+
 
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
-        self.shapesize(1, 4, 0)
-        self.goto(240, 0)
-        self.color("white")
         self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(1, 4, 0)
+        self.setheading(90)
+        self.speed(SPEED)
+        self.goto(240, 0)
 
+    def up(self):
+        self.forward(10)
+
+    def down(self):
+        self.forward(-10)
