b8f517f21a21360d2a710e933c560ca1e11ee354
Xor01 <ms3ccc71@gmail.com>
2022-10-03 16:19:29 +0300

    Day 24
    Final Project, Mail Merger

--- Day24/main.py
+++ Day24/main.py
@@ -1,6 +1,19 @@
-# file = open("file.txt")
-# content = file.read()
-# print(content)
-with open("file.txt") as file:
-    content = file.read()
-    print(content)
+# Mail Merge
+
+PLACEHOLDER = "[name]"
+
+
+with open("Input/Letters/starting_letter.txt") as letter:
+    new_letter = letter.read()
+
+
+with open("Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+
+for name in names:
+    formatted_name = name.strip()
+    with open(f"Output/ReadyToSend/letter_for_{formatted_name}.txt", 'w') as out:
+        name_letter = new_letter
+        name_letter = name_letter.replace(PLACEHOLDER, formatted_name)
+        out.write(name_letter)
