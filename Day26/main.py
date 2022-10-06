8d449ca1512b8a2c2ba8e39194333f28c5970ac8
Xor01 <ms3ccc71@gmail.com>
2022-10-06 17:42:59 +0300

    Day 26
    NATO alpha

--- Day26/main.py
+++ Day26/main.py
@@ -1,1 +1,14 @@
+# NATO alphabet converter
+
 import pandas
+
+user_inp = input("Enter a word or a sentence: ").upper()
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data = pandas.DataFrame(data)
+
+
+letter_list = {row.letter: row.code for (index, row) in data.iterrows()}
+NATA_alpha = [letter_list[letter] for letter in user_inp if letter in letter_list.keys()]
+
+print(NATA_alpha)
