8cb06357cefb9108f6b4dd41cc15792c2a5b15ad
Xor01 <ms3ccc71@gmail.com>
2022-10-07 13:21:52 +0300

    Day 28
    Pomodoro timer starter code

---
+++ Day28/main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from PIL import ImageTk, Image
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_photo = ImageTk.PhotoImage(Image.open("tomato.png"))
+canvas.create_image(100, 112, image=tomato_photo)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+canvas.pack()
+
+window.mainloop()
