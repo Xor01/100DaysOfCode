87d8273d6928966c70f7b62033bbbd79df9c07c7
Xor01 <ms3ccc71@gmail.com>
2022-10-07 13:32:58 +0300

    Finish UI
    add labels and their positions

--- Day28/main.py
+++ Day28/main.py
@@ -17,14 +17,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window part
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Label Timer
+
+timer_label = Label(text="Timer", fg=GREEN)
+timer_label.grid(row=0, column=2)
+timer_label.config(font=(FONT_NAME, 40, "bold"))
+
+
+# Canvas part
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_photo = ImageTk.PhotoImage(Image.open("tomato.png"))
 canvas.create_image(100, 112, image=tomato_photo)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
-canvas.pack()
+canvas.grid(row=2, column=2)
+
+# Start Label
+start_label = Label(text="Start", fg="black")
+start_label.grid(row=3, column=1)
+
+# Reset Label
+reset_label = Label(text="Reset", fg="black")
+reset_label.grid(row=3, column=3)
+
 
 window.mainloop()
