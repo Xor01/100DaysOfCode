3b272923116060163c9e5d00a26a88790bb49ea5
Xor01 <ms3ccc71@gmail.com>
2022-10-08 15:59:18 +0300

    save password function

--- Day29/main.py
+++ Day29/main.py
@@ -1,8 +1,16 @@
 from tkinter import *
 from PIL import ImageTk, Image
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    with open("password.txt", mode="a") as pass_file:
+        pass_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,13 +30,15 @@ website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus()
 
 # Email Component
 email_label = Label(text="Email/user Name")
 email_label.grid(row=2, column=0)
 
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW", )
+email_entry.insert(0, string="m@m.com")
 
 # Password Component
 password_label = Label(text="Password")
@@ -36,13 +46,12 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
-
 # Password Button
 password_button = Button(text="Generate Password")
 password_button.grid(row=3, column=2, sticky="EW")
 
 # Add Button
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
