13e82d011cc55b2fd4cea3baea006d17741c1246
Xor01 <ms3ccc71@gmail.com>
2022-10-13 14:50:28 +0300

    ui finished

--- Day31/main.py
+++ Day31/main.py
@@ -13,15 +13,24 @@ window.title("Flash Card")
 
 # Canvas
 canvas = Canvas(width=800, height=526)
-my_image = ImageTk.PhotoImage(Image.open("images/card_front.png"))
-canvas.create_image(400, 263, image=my_image)
+canvas_image = ImageTk.PhotoImage(Image.open("images/card_front.png"))
+canvas.create_image(400, 263, image=canvas_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 # Canvas Text
 canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
 canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 
+# Unknown Image
+cross_image = ImageTk.PhotoImage(Image.open("images/wrong.png"))
+wrong_button = Button(image=cross_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
 
+
+# Correct Image
+check_image = ImageTk.PhotoImage(Image.open("images/right.png"))
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 window.mainloop()
