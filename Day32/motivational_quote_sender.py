7fdb1d99854d395042033bc1c64beb9ed21cb701
Xor01 <ms3ccc71@gmail.com>
2022-10-14 15:23:00 +0300

    motivational quote sender

--- Day32/motivational_quote_sender.py
+++ Day32/motivational_quote_sender.py
@@ -4,7 +4,7 @@ from SMTPInfo import *
 import random
 
 
-def send_birthday_wish(msg):
+def send_motivational_quote(msg):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=email, password=password)
@@ -13,3 +13,10 @@ def send_birthday_wish(msg):
             to_addrs="tomesoh800@dicopto.com",
             msg=f"Subject: Good Morning\n\n{msg}"
         )
+
+
+with open("quotes.txt") as file:
+    data = file.readlines()
+    quote = random.choice(data)
+    if dt.datetime.now().weekday() == 4:
+        send_motivational_quote(quote)
