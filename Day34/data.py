9f477bfecd9af9a40bb3cd5a6ae3523089c0e98e
Xor01 <ms3ccc71@gmail.com>
2022-10-16 19:28:39 +0300

    Day 34
    Adding try except for API call

--- Day34/data.py
+++ Day34/data.py
@@ -4,9 +4,11 @@ params = {
     "amount": 10,
     "type": "boolean"
 }
-dt = requests.get("https://opentdb.com/api.php", params=params)
-dt.raise_for_status()
-
-data = dt.json()["results"]
-
-question_data = data
+try:
+    dt = requests.get("https://opentdb.com/api.php", params=params)
+    dt.raise_for_status()
+except:
+    print("Something happened, could not fetch data.")
+else:
+    data = dt.json()["results"]
+    question_data = data
