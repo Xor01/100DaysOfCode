b89a2e47216e56144916d148b4d63afde9ef54ca
Xor01 <ms3ccc71@gmail.com>
2022-10-16 19:09:09 +0300

    Day 34
    Final Quiz App

--- Day34/ui.py
+++ Day34/ui.py
@@ -25,15 +25,37 @@ class QuizInterface:
         check_img = ImageTk.PhotoImage(Image.open("images/true.png"))
         cross_img = ImageTk.PhotoImage(Image.open("images/false.png"))
 
-        self.check = Button(text="Cross", image=check_img,highlightthickness=0)
+        self.check = Button(text="Cross", image=check_img, highlightthickness=0, bd=0, command=self.send_true_check)
         self.check.grid(row=2, column=0)
 
-        self.cross = Button(text="Cross", image=cross_img, highlightthickness=0)
+        self.cross = Button(text="Cross", image=cross_img, highlightthickness=0, bd=0, command=self.send_false_check)
         self.cross.grid(row=2, column=1)
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.q_text, text=q_text)
+        self.canvas.config(bg="white")
+        self.score.config(text=f"Score: {self.quiz.score}")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.q_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.q_text, text="You reached the final question")
+            self.check.config(state="disabled")
+            self.cross.config(state="disabled")
+
+    def send_true_check(self):
+        ans = self.quiz.check_answer("true")
+        self.give_feedback(ans)
+
+    def send_false_check(self):
+        ans = self.quiz.check_answer("false")
+        self.give_feedback(ans)
+
+    def give_feedback(self, ans):
+        if ans:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
