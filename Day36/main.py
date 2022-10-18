1f0e0fad9049b64e9deab7387e47102f95bec3d1
Xor01 <ms3ccc71@gmail.com>
2022-10-18 18:09:00 +0300

    Finsh getting the difference between yesterday and the day before

--- Day36/main.py
+++ Day36/main.py
@@ -1,8 +1,40 @@
+from newsapi import NewsApiClient
+import requests
+import os
+import datetime
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+url = 'https://www.alphavantage.co/query'
+stocks_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": os.environ.get("ALPHA_KEY")
+}
+
+news_params = {
+    "keyword": "Tesla",
+    "symbol": STOCK,
+    "apikey": os.environ.get("ALPHA_KEY")
+}
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(days=1)
+before_yesterday = today - datetime.timedelta(days=2)
+
+response = requests.get(url=url, params=stocks_params)
+data = response.json()["Time Series (Daily)"]
+
+days = [value for (day, value) in data.items()]
+
+yesterday_stock = days[0]
+before_yesterday_stock = days[1]
+difference = float(yesterday_stock["4. close"]) - float(before_yesterday_stock["4. close"])
+
+if (difference / float(yesterday_stock["4. close"])) * 100 > 5:
+    print("Get news")
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
