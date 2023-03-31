92ef24f546abdea5a3864153debb093f0149f4f8
Xor01 <ms3ccc71@gmail.com>
2023-03-31 17:25:10 +0300

    final Linkedin job saver

--- Day49/main.py
+++ Day49/main.py
@@ -3,17 +3,20 @@ from os import getenv
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from time import sleep
+
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
-service = Service(executable_path="C://dev//chromedriver.exe")
+service = Service(executable_path="C:\\dev\\chromedriver.exe")
+
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.maximize_window()
 email = getenv("EMAIL")
 password = getenv("PASS")
 
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3468021938&f_LF=f_AL&geoId=100459316&keywords=programming+intern&location=Saudi+Arabia&refresh=true")
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3522669020&f_AL=true&geoId=100459316&keywords=python%20developer%20&location=Saudi%20Arabia&refresh=true")
 
 # sign in button in the jobs page
 driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a[2]").click()
@@ -29,4 +32,19 @@ driver.implicitly_wait(3)
 # jobs_list = driver.find_element(By.CSS_SELECTOR, ".job-card-container")
 # jobs_list.click()
 
-driver.find_element(By.XPATH, '//*[@id="main"]/div/section[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[3]/div/button').click()
+driver.find_element(By.XPATH, '//*[@id="main"]/div/section[2]/div/div[2]/div[1]/div/div[1]/'
+                              'div/div[1]/div[1]/div[3]/div/button').click()
+
+sleep(5)
+
+all_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+
+for listing in all_listings:
+    print("called")
+    listing.click()
+    sleep(2)
+    try:
+        listing.find_element(By.LINK_TEXT, "Save").click()
+        sleep(5)
+    except (Exception):
+        pass
