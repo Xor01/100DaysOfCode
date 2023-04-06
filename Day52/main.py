a92144c0c1a4bcc48dbfa71b82380aa42c0c297b
Xor01 <ms3ccc71@gmail.com>
2023-04-06 18:27:09 +0300

    creating selenium base code

--- Day52/main.py
+++ Day52/main.py
@@ -1,1 +1,15 @@
 # Date Entry Job Automation
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+
+service = Service(executable_path="C:\\dev\\chromedriver.exe")
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=service, options=options)
+driver.maximize_window()
+
+driver.get("")
