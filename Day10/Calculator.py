b9262536b2f5344351e21cb8da934425af17c271
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-08-11 00:25:15 +0300

    add Day 10 project name comment

--- Day10/Calculator.py
+++ Day10/Calculator.py
@@ -1,44 +1,54 @@
+# Calculator
 from art import logo
-def multiplay(n1,n2):
+
+
+def multiply(n1, n2):
     return n1 * n2
 
-def divide(n1,n2):
+
+def divide(n1, n2):
     return n1 / n2
 
-def add(n1,n2):
+
+def add(n1, n2):
     return n1 + n2
 
-def subtract(n1,n2):
+
+def subtract(n1, n2):
     return n1 - n2
-    
+
+
 operations = {
-    "+" : add,
-    '-' : subtract,
-    '*' : multiplay,
-    '/' : divide,
+    "+": add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
 }
+
 
 def calculator():
     print(logo)
     n1 = float(input("What is the first number: "))
     program_continue = 'y'
-    
+
     for key in operations:
         print(key)
-    
+
     while program_continue == 'y':
         operation = input("Pick an operation: ")
-        
+
         n2 = float(input("What is the next number: "))
-        
-        answer = operations[operation](n1,n2)
-        
+
+        answer = operations[operation](n1, n2)
+
         print(f"{n1} {operation} {n2} = {answer}")
         program_continue = input("Would you like to continue calculating 'y' or 'n' to new start or 'e' to exit: ")
         n1 = answer
-    
+
     if program_continue == 'n':
         calculator()
     elif program_continue == 'e':
         return
+
+
 calculator()
