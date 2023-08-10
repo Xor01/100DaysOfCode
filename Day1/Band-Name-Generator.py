cae842633d26efae6704b4544ed6070f66d8ce0b
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-08-11 00:16:51 +0300

    add Day 1 project name comment

--- Day1/Band-Name-Generator.py
+++ Day1/Band-Name-Generator.py
@@ -1,8 +1,9 @@
 # Created By Mohammed at Sep, 1 2022
+# Band-Name-Generator
 from curses import init_pair
 
 
-print("Welcom to Band Name Generator")
+print("Welcome to Band Name Generator")
 
 city = input("Enter city you grew up in:\n")
 petName = input("Enter your pet name:\n")
