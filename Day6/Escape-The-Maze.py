e758f73712209fa17fa58efb5dc81d0d14b36fbf
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-08-11 00:21:55 +0300

    add Day 6 project name comment

--- Day6/Escape-The-Maze.py
+++ Day6/Escape-The-Maze.py
@@ -1,4 +1,5 @@
-#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# Escape the Maze Game
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 # def turn_right():
 #     turn_left()
 #     turn_left()
