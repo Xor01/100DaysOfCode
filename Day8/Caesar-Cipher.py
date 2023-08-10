545d885d875df0152181177e1312cdad6986c1ac
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-08-11 00:23:41 +0300

    add Day 8 project name comment

--- Day8/Caesar-Cipher.py
+++ Day8/Caesar-Cipher.py
@@ -1,23 +1,26 @@
-#TODO-1: Import and print the logo from art.py when the program starts.
+# Ceaser-Sipher
+# TODO-1: Import and print the logo from art.py when the program starts.
 from art import logo
+
 print(logo)
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-symbols = [' ',',','.','!','@','#','$','$','%','^','?']
+symbols = [' ', ',', '.', '!', '@', '#', '$', '$', '%', '^', '?']
+
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
     for char in start_text:
-        #TODO-3: What happens if the user enters a number/symbol/space?
-       if char in symbols:
-           position = symbols.index(char)
-           end_text += symbols[position]
-       else:
+        # TODO-3: What happens if the user enters a number/symbol/space?
+        if char in symbols:
+            position = symbols.index(char)
+            end_text += symbols[position]
+        else:
             position = alphabet.index(char)
             new_position = ((position + shift_amount) % 26)
             end_text += alphabet[new_position]
@@ -25,12 +28,12 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 
 start = "yes"
-while (start == "yes"):
+while start == "yes":
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
